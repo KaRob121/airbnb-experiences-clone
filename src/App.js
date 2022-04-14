4be0385fa731d8c62f75e1131f2e294c8bba4c13
Karl <24803299+KaRob121@users.noreply.github.com>
2022-04-14 15:46:24 -0700

    Using JS map function to render data from a file as components

--- src/App.js
+++ src/App.js
@@ -2,20 +2,32 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card'
+import data from './data.js'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card 
+      {data.map( experience =>
+        <Card 
+          img={experience.coverImg}
+          rating={experience.stats.rating}
+          reviewCount={experience.stats.reviewCount}
+          country={experience.location}
+          title={experience.title}
+          price={experience.price}
+
+        />
+      )}
+      {/* <Card 
         img="katie-zaferes.png"
         rating={5.0}
         reviewCount={6}
         country="USA"
         title="Life lessons with Katie Zaferes"
         price={136}
-      />
+      /> */}
     </div>
   );
 }
