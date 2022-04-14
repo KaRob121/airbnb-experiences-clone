67a1eae8773da8df25b1ad37db952f8292e9c5ce
Karl <24803299+KaRob121@users.noreply.github.com>
2022-04-14 15:54:36 -0700

    Refactored data mapping code

--- src/App.js
+++ src/App.js
@@ -5,29 +5,23 @@ import Card from './components/Card'
 import data from './data.js'
 
 function App() {
+  const cards = data.map( experience => {
+    return (
+      <Card 
+        img={experience.coverImg}
+        rating={experience.stats.rating}
+        reviewCount={experience.stats.reviewCount}
+        country={experience.location}
+        title={experience.title}
+        price={experience.price}
+      />
+    )
+  })
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      {data.map( experience =>
-        <Card 
-          img={experience.coverImg}
-          rating={experience.stats.rating}
-          reviewCount={experience.stats.reviewCount}
-          country={experience.location}
-          title={experience.title}
-          price={experience.price}
-
-        />
-      )}
-      {/* <Card 
-        img="katie-zaferes.png"
-        rating={5.0}
-        reviewCount={6}
-        country="USA"
-        title="Life lessons with Katie Zaferes"
-        price={136}
-      /> */}
+      {cards}
     </div>
   );
 }
