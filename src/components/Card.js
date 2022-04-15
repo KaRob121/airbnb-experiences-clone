c2554c4cab9015a063d1084b125ca805e51f9414
Karl <24803299+KaRob121@users.noreply.github.com>
2022-04-14 17:09:33 -0700

    Alternate method of passing in data as a prop using spread operator; less verbose, but could also be less readable; completed project

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,24 +3,30 @@ import star from '../images/Star 1.png'
 function Card(props) {
   let cardStatus
 
-  if (props.item.openSpots === 0) {
+  if (props.openSpots === 0) {
     cardStatus = "SOLD OUT"
-  } else if (props.item.location === "Online") {
+  } else if (props.location === "Online") {
     cardStatus = "ONLINE"
   }
 
   return (
     <div className="card">
       <div className="card--image-section">
-        <img className="card--image" src={`./images/${ props.item.coverImg }`} />
-        { cardStatus && <p className="card--status">{ cardStatus }</p> }
+        <img 
+          className="card--image" 
+          src={`./images/${ props.coverImg }`} 
+        />
+        { 
+          cardStatus &&
+          <p className="card--status">{ cardStatus }</p> 
+        }
       </div>
       <div className="card--rating-text">
         <img className="card--star" src={star} />
-        <p>{ props.item.stats.rating.toFixed(1) } ({ props.item.stats.reviewCount }) · { props.item.location }</p>
+        <p>{ props.stats.rating.toFixed(1) } ({ props.stats.reviewCount }) · { props.location }</p>
       </div>
-      <p className="card--title"> {props.item.title }</p>
-      <p className="card--price"><strong>From ${ props.item.price }</strong> / person</p>
+      <p className="card--title"> { props.title }</p>
+      <p className="card--price"><strong>From ${ props.price }</strong> / person</p>
     </div>
   ) 
 }
